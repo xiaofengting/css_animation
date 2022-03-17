71e14917508b30930318c775e21c64aaed128978
hejinfei <hejinfei@360.cn>
2022-03-18 00:36:08 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+import { ref } from 'vue';
+import HomeStage from './components/HomeStage.vue'
+
+const loading = ref(true)
+const menu = ref(false)
+
+function restart() {
+  menu.value = false
+  loading.value = true
+}
+function showMenu() {
+  menu.value = true
+  loading.value = false
+}
+</script>
+
+<template>
+  <HomeStage v-if="loading" />
+  <button type="button" class="last-chapter__menu-btn" @click="showMenu">
+    <div></div>
+    <div></div>
+    <div></div>
+  </button>
+  <div class="menu" :class="{ active: menu }">
+    <section class="menu__box">
+      <div class="bg"></div>
+    </section>
+    <button type="button" class="menu__close-btn" @click="restart">
+      <div></div>
+      <div></div>
+    </button>
+  </div>
+</template>
+
---
+++ src/components/HomeStage.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import './HomeStage.css'
+</script>
+
+<template>
+  <main id="stage">
+    <div class="motion-type-1">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+    <div class="motion-type-2">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+    <div class="motion-type-3">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+</style>
